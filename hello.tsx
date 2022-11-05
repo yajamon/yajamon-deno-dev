f056efbb9ee551776955d6b26a4a0c66ebf53805
yajamon <yajamon.tatsuki@gmail.com>
2022-11-06 03:17:19 +0900

    JSXで出力してみる

--- hello.tsx
+++ hello.tsx
@@ -1,9 +1,23 @@
+
 import { serve } from "https://deno.land/std@0.162.0/http/server.ts";
+import { h, renderSSR } from "https://deno.land/x/nano_jsx@v0.0.34/mod.ts";
+
+const content = "Hello World! Hello Deno! Hello JSX!";
+const App = () => (
+  <html>
+    <head>
+      <title>hello deno deploy</title>
+    </head>
+    <body>
+      <div>{content}</div>
+    </body>
+  </html>
+);
 
 serve((_req) => {
-  const content = "Hello World! Hello Deno!";
-  const response = new Response(content, {
-    headers: { "content-type": "text/plain" },
+  const html = renderSSR(<App />);
+  const response = new Response(html, {
+    headers: { "content-type": "text/html" },
   });
 
   return response;
