bf2216aa8e5a61a8757c6d9ee80b92d4aab32d6f
yajamon <yajamon.tatsuki@gmail.com>
2022-11-06 02:41:49 +0900

    Update module http/server

--- hello.ts
+++ hello.ts
@@ -1,4 +1,4 @@
-import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.162.0/http/server.ts";
 
 serve((_req) => {
   const content = "Hello World! Hello Deno!";
