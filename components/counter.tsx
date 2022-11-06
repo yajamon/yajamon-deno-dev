5792333e0cda3a69747ffd5f29e7e62a9eb46232
yajamon <yajamon.tatsuki@gmail.com>
2022-11-06 13:23:00 +0900

    CounterClass をコンポーネントとして分離する

---
+++ components/counter.tsx
@@ -0,0 +1,24 @@
+/** @jsx h */
+/// <reference no-default-lib="true"/>
+/// <reference lib="dom" />
+/// <reference lib="dom.asynciterable" />
+/// <reference lib="deno.ns" />
+
+import { h, Component } from "https://deno.land/x/nano_jsx@v0.0.34/mod.ts";
+
+export class CounterClass extends Component {
+  count = 0;
+  countUp() {
+    this.count += 1;
+    this.update();
+  }
+
+  render(){
+    return (
+      <div>
+        「{this.count}」
+        <button onClick={()=>this.countUp()} >Count Up</button>
+      </div>
+    );
+  }
+}
--- hello.tsx
+++ hello.tsx
@@ -6,23 +6,7 @@
 
 import { serve } from "https://deno.land/std@0.162.0/http/server.ts";
 import { h, renderSSR, Component } from "https://deno.land/x/nano_jsx@v0.0.34/mod.ts";
-
-class CounterClass extends Component {
-  count = 0;
-  countUp() {
-    this.count += 1;
-    this.update();
-  }
-
-  render(){
-    return (
-      <div>
-        「{this.count}」
-        <button onClick={()=>this.countUp()} >Count Up</button>
-      </div>
-    );
-  }
-}
+import { CounterClass } from "./components/counter.tsx"
 
 const content = "Hello World! Hello Deno! Hello JSX!";
 const App = () => (
