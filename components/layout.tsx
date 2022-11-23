4cb05ac8db9d9815c7fcf763333935b60f03c515
yajamon <yajamon.tatsuki@gmail.com>
2022-11-23 17:17:48 +0900

    リンクをタグ化したり、分岐して得るものを Fragment に包んだりする

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,12 +1,13 @@
-import { h, renderSSR, Component } from "https://deno.land/x/nano_jsx@v0.0.34/mod.ts";
+import { h, Fragment, renderSSR, Component } from "https://deno.land/x/nano_jsx@v0.0.34/mod.ts";
 
 const ENVKEY_REPO_URL = "GITHUB_REPO_URL";
 
 export class Footer extends Component {
   render() {
-    const repoUrl = Deno.env.get(ENVKEY_REPO_URL) || "!!環境変数からの読み込みに失敗!!"
+    const repoUrl = Deno.env.get(ENVKEY_REPO_URL);
+    const repoUrlContent = repoUrl ? (<Fragment>Repository URL: <a href={repoUrl}>{repoUrl}</a></Fragment>) : (<Fragment>!!環境変数からの読み込みに失敗!!</Fragment>);
     return (
-      <div>Repository URL: {repoUrl}</div>
+      <div>{repoUrlContent}</div>
     )
   }
 }
